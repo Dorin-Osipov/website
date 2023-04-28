2964930d7f5066d9bb46409e049c7e39ead3a23a
Dorin-Osipov <dorinosipd@gmail.com>
2023-04-28 11:54:28 +0100

    complete personal portofolio

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let contact_btn = document.getElementById("contact-btn");
+let contact_icons = document.getElementById("contact-icons")
+
+function addContactIcons() {
+contact_icons.style.visibility ="visible"
+}
+
+contact_btn.onclick = addContactIcons;
